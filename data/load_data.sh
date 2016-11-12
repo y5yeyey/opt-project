cf90a8ca9f596fb57c370417d6114304f6a07c2a
Congwei Wu <congweiwu@Congweis-MacBook-Pro.local>
2016-11-12 00:31:15 -0800

    add load_data shell script

---
+++ data/load_data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python sql.py
+
+mysql -u"root" "talkingdata" < db.sql
+mysql -u"root" "talkingdata" < app_labels.sql
+mysql -u"root" "talkingdata" < events.sql
+mysql -u"root" "talkingdata" < gender_age_test.sql
+mysql -u"root" "talkingdata" < gender_age_train.sql
+mysql -u"root" "talkingdata" < label_categories.sql
+mysql -u"root" "talkingdata" < phone_brand_device_model.sql
